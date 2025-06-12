9c8bd18f4fa636167dbefe3fe4bc2f93e94dec65
Yi LIU <yiliu@zzu.edu.cn>
2025-06-12 15:21:54 +0800

    merge daqb into frontend

--- frontend/src/Frontend.cc
+++ frontend/src/Frontend.cc
@@ -3,8 +3,28 @@
 #include <regex>
 #include <filesystem>
 #include <array>
+#include <iostream>
+#include <thread>
 
 #include "rbcp.hh"
+#include "TcpConnection.hh"
+
+
+#pragma GCC diagnostic ignored "-Wpmf-conversions"
+
+
+#ifndef DEBUG_PRINT
+#define DEBUG_PRINT 0
+#endif
+#define debug_print(fmt, ...)                                           \
+  do { if (DEBUG_PRINT) std::fprintf(stdout, fmt, ##__VA_ARGS__); } while (0)
+
+#ifndef INFO_PRINT
+#define INFO_PRINT 0
+#endif
+#define info_print(fmt, ...)                                           \
+  do { if (INFO_PRINT) std::fprintf(stdout, fmt, ##__VA_ARGS__); } while (0)
+
 
 
 Frontend::Frontend(const std::string& netip){
@@ -488,3 +508,196 @@ std::set<std::pair<uint16_t, uint16_t>> Frontend::ReadPixelMask_from_file(const 
 
 //===============================================
 
+
+
+
+Frontend::~Frontend(){
+  m_tcpcon.reset();
+
+  m_is_async_watching = false;
+  if(m_fut_async_watch.valid())
+    m_fut_async_watch.get();
+}
+
+void Frontend::daq_start_run(){
+  m_vec_ring_ev.clear();
+  m_vec_ring_ev.resize(m_size_ring);
+  m_count_ring_write = 0;
+  m_count_ring_read = 0;
+  m_hot_p_read = m_size_ring -1; // tail
+
+  m_flag_wait_first_event = true;
+
+  m_st_n_tg_ev_now =0;
+  m_st_n_ev_input_now =0;
+  m_st_n_ev_output_now =0;
+  m_st_n_ev_bad_now =0;
+  m_st_n_ev_overflow_now =0;
+  m_st_n_tg_ev_begin = 0;
+
+  //rbcp start
+  m_isDataAccept= true;
+
+  m_tcpcon =  TcpConnection::connectToServer(m_netip,  24, reinterpret_cast<FunProcessMessage>(&Frontend::perConnProcessRecvMesg), nullptr, this);
+
+  if(!m_is_async_watching){
+    m_fut_async_watch = std::async(std::launch::async, &Frontend::AsyncWatchDog, this);
+  }
+
+  return;
+}
+
+void Frontend::daq_stop_run(){
+  m_isDataAccept= false;
+
+  m_is_async_watching = false;
+  if(m_fut_async_watch.valid()){
+    m_fut_async_watch.get();
+  }
+
+  return;
+}
+
+void Frontend::daq_reset(){
+  m_isDataAccept= false;
+  return;
+}
+
+
+void Frontend::daq_conf_default(){
+
+  return;
+}
+
+int Frontend::perConnProcessRecvMesg(void* pconn, const std::string& str){
+  static size_t s_n = 0;
+  if(!m_isDataAccept){
+    std::cout<< "msg is dropped"<<std::endl;
+    return 0;
+  }
+
+  daqb_packSP df(new DataPack);
+
+  df->MakeDataPack(str);
+  s_n ++;
+
+  m_st_n_ev_input_now ++;
+  uint64_t next_p_ring_write = m_count_ring_write % m_size_ring;
+  if(next_p_ring_write == m_hot_p_read){
+    // buffer full, permanent data lose
+    m_st_n_ev_overflow_now ++;
+    return 0;
+  }
+
+  m_vec_ring_ev[next_p_ring_write] = df;
+  m_count_ring_write ++;
+  return 1;
+}
+
+std::string Frontend::GetStatusString(){
+  std::unique_lock<std::mutex> lk(m_mtx_st);
+  return m_st_string;
+}
+
+daqb_packSP& Frontend::Front(){
+  if(m_count_ring_write > m_count_ring_read) {
+    uint64_t next_p_ring_read = m_count_ring_read % m_size_ring;
+    m_hot_p_read = next_p_ring_read;
+    // keep hot read to prevent write-overlapping
+    return m_vec_ring_ev[next_p_ring_read];
+  }
+  else{
+    return m_ring_end;
+  }
+}
+
+void Frontend::PopFront(){
+  if(m_count_ring_write > m_count_ring_read) {
+    uint64_t next_p_ring_read = m_count_ring_read % m_size_ring;
+    m_hot_p_read = next_p_ring_read;
+    // keep hot read to prevent write-overlapping
+    m_vec_ring_ev[next_p_ring_read].reset();
+    m_count_ring_read ++;
+  }
+}
+
+uint64_t Frontend::Size(){
+  return  m_count_ring_write - m_count_ring_read;
+}
+
+void Frontend::ClearBuffer(){
+  m_count_ring_write = m_count_ring_read;
+  m_vec_ring_ev.clear();
+}
+
+uint64_t Frontend::AsyncWatchDog(){
+  std::chrono::system_clock::time_point m_tp_old;
+  std::chrono::system_clock::time_point m_tp_run_begin;
+
+  m_tp_run_begin = std::chrono::system_clock::now();
+  m_tp_old = m_tp_run_begin;
+  m_is_async_watching = true;
+
+  m_st_n_tg_ev_old =0;
+  m_st_n_ev_input_old = 0;
+  m_st_n_ev_bad_old =0;
+  m_st_n_ev_overflow_old = 0;
+
+  while(m_is_async_watching){
+    std::this_thread::sleep_for(std::chrono::seconds(1));
+    uint64_t st_n_tg_ev_begin = m_st_n_tg_ev_begin;
+    uint64_t st_n_tg_ev_now = m_st_n_tg_ev_now;
+    uint64_t st_n_ev_input_now = m_st_n_ev_input_now;
+    uint64_t st_n_ev_bad_now = m_st_n_ev_bad_now;
+    uint64_t st_n_ev_overflow_now = m_st_n_ev_overflow_now;
+
+    // time
+    auto tp_now = std::chrono::system_clock::now();
+    std::chrono::duration<double> dur_period_sec = tp_now - m_tp_old;
+    std::chrono::duration<double> dur_accu_sec = tp_now - m_tp_run_begin;
+    double sec_period = dur_period_sec.count();
+    double sec_accu = dur_accu_sec.count();
+
+    // period
+    uint64_t st_n_tg_ev_period = st_n_tg_ev_now - m_st_n_tg_ev_old;
+    uint64_t st_n_ev_input_period = st_n_ev_input_now - m_st_n_ev_input_old;
+    uint64_t st_n_ev_bad_period = st_n_ev_bad_now - m_st_n_ev_bad_old;
+    uint64_t st_n_ev_overflow_period = st_n_ev_overflow_now - m_st_n_ev_overflow_old;
+
+    // ratio
+    //double st_output_vs_input_accu = st_n_ev_input_now? st_ev_output_now / st_ev_input_now : 1;
+    double st_bad_vs_input_accu = st_n_ev_input_now? 1.0 * st_n_ev_bad_now / st_n_ev_input_now : 0;
+    double st_overflow_vs_input_accu = st_n_ev_input_now? 1.0 *  st_n_ev_overflow_now / st_n_ev_input_now : 0;
+    double st_input_vs_trigger_accu = st_n_ev_input_now? 1.0 * st_n_ev_input_now / (st_n_tg_ev_now - st_n_tg_ev_begin + 1) : 1;
+    //double st_output_vs_input_period = st_ev_input_period? st_ev_output_period / st_ev_input_period : 1;
+    double st_bad_vs_input_period = st_n_ev_input_period? 1.0 * st_n_ev_bad_period / st_n_ev_input_period : 0;
+    double st_overflow_vs_input_period = st_n_ev_input_period? 1.0 *  st_n_ev_overflow_period / st_n_ev_input_period : 0;
+    double st_input_vs_trigger_period = st_n_tg_ev_period? 1.0 *  st_n_ev_input_period / st_n_tg_ev_period : 1;
+
+    // hz
+    double st_hz_tg_accu = (st_n_tg_ev_now - st_n_tg_ev_begin + 1) / sec_accu ;
+    double st_hz_input_accu = st_n_ev_input_now / sec_accu ;
+
+    double st_hz_tg_period = st_n_tg_ev_period / sec_period ;
+    double st_hz_input_period = st_n_ev_input_period / sec_period ;
+
+    std::string st_string_new =
+      FormatString("L<%u> event(%d)/trigger(%d - %d)=Ev/Tr(%.4f) dEv/dTr(%.4f) tr_accu(%.2f hz) ev_accu(%.2f hz) tr_period(%.2f hz) ev_period(%.2f hz)",
+                             m_extension, st_n_ev_input_now, st_n_tg_ev_now, st_n_tg_ev_begin, st_input_vs_trigger_accu, st_input_vs_trigger_period,
+                             st_hz_tg_accu, st_hz_input_accu, st_hz_tg_period, st_hz_input_period
+        );
+
+    {
+      std::unique_lock<std::mutex> lk(m_mtx_st);
+      m_st_string = std::move(st_string_new);
+    }
+
+    //write to old
+    m_st_n_tg_ev_old = st_n_tg_ev_now;
+    m_st_n_ev_input_old = st_n_ev_input_now;
+    m_st_n_ev_bad_old = st_n_ev_bad_now;
+    m_st_n_ev_overflow_old = st_n_ev_overflow_now;
+    m_tp_old = tp_now;
+  }
+  return 0;
+}
